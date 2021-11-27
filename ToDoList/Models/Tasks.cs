338a60410ebc89da6802ef2c04ade28eb383a54a
unknown <andji.gash@gmail.com>
2021-11-27 12:36:18 +0100

    Added project

---
+++ ToDoList/Models/Tasks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ToDoList.Models
+{
+    public partial class Tasks
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime AddedOn { get; set; }
+    }
+}
---
+++ ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ToDoList.Models;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ToDoList.Models
+{
+    public partial class ToDoListContext : DbContext
+    {
+        public ToDoListContext()
+        {
+        }
+
+        public ToDoListContext(DbContextOptions<ToDoListContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Tasks> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-3OL9D3J;Database=ToDoList;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tasks>(entity =>
+            {
+                entity.Property(e => e.AddedOn)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Content).IsUnicode(false);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public DbSet<ToDoList.Models.Tasks> Task { get; set; }
+    }
+}
